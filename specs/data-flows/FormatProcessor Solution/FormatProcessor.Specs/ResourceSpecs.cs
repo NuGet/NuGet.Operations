e6adfc52ec3de2d79d50a9289f5c6f281f4de793
Howard Dierking <howard@microsoft.com>
2013-10-30 15:52:50 -0700

    refactored loader functions

--- specs/data-flows/FormatProcessor Solution/FormatProcessor.Specs/ResourceSpecs.cs
+++ specs/data-flows/FormatProcessor Solution/FormatProcessor.Specs/ResourceSpecs.cs
@@ -5,7 +5,7 @@ using Machine.Specifications;
 namespace FormatProcessor.Specs
 {
     class When_loading_a_resource_from_a_string {
-        Because of = () => resource = Resource.LoadString(strings.nuget_index_json);
+        Because of = () => resource = Resource.Load(strings.nuget_index_json);
         It should_return_non_null_object = () => resource.ShouldNotBeNull();
         It should_have_a_valid_root = () => resource.Root.ShouldNotBeNull();
        
@@ -13,14 +13,14 @@ namespace FormatProcessor.Specs
     }
 
     class When_attempting_to_load_invalid_json {
-        Because of = () => exception = Catch.Exception(() => Resource.LoadString(strings.nuget_index_invalid_json));
+        Because of = () => exception = Catch.Exception(() => Resource.Load(strings.nuget_index_invalid_json));
         It should_throw = () => exception.ShouldNotBeNull();
         It should_throw_FormatException = () => exception.ShouldBeOfType<FormatException>(); // we don't want to pass through a json.net exception as it would require consumers to have a direct dependency on json.net types
         static Exception exception;
     }
 
     class When_attempting_to_load_null {
-        Because of = () => exception = Catch.Exception(() => Resource.LoadString(null));
+        Because of = () => exception = Catch.Exception(() => Resource.Load((string) null));
         It should_throw = () => exception.ShouldNotBeNull();
         It should_throw_ArgumentNullException = () => exception.ShouldBeOfType<ArgumentNullException>();
         static Exception exception;
@@ -31,7 +31,7 @@ namespace FormatProcessor.Specs
             url = "https://raw.github.com/NuGet/NuGetApi/master/specs/data-flows/index.json";
         };
         Because of = () => {
-            resource = Resource.LoadUrl(new Uri(url));
+            resource = Resource.Load(new Uri(url));
         };
         It should_execute_http_request;
         It should_return_non_null_object = () => resource.ShouldNotBeNull();
--- specs/data-flows/FormatProcessor Solution/FormatProcessor/Resource.cs
+++ specs/data-flows/FormatProcessor Solution/FormatProcessor/Resource.cs
@@ -11,7 +11,7 @@ namespace FormatProcessor {
             _jsonDoc = jsonDoc;
         }
 
-        public static Resource LoadString(string data) {
+        public static Resource Load(string data) {
             if(data == null)
                 throw new ArgumentNullException("data");
             try {
@@ -25,15 +25,15 @@ namespace FormatProcessor {
             }
         }
 
-        public static Resource LoadUrl(Uri uri) {
+        public static Resource Load(Uri uri) {
             // todo: examine the uri scheme and switch resolver to a UNC file resolver
             // todo: refactor HttpLinkResolver creation to a dependency resolver that can be mocked
-            return LoadUrl(uri, new HttpLinkResolver());
+            return Load(uri, new HttpLinkResolver());
         }
 
-        public static Resource LoadUrl(Uri url, ILinkResolver resolver) {
+        public static Resource Load(Uri url, ILinkResolver resolver) {
             var resourceString = resolver.Get(url);
-            return LoadString(resourceString);
+            return Load(resourceString);
         }
 
         public object Root {
