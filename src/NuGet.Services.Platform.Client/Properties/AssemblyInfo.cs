50a588eb348db58529e128b08fda37c780beeffc
anurse <andrew@andrewnurse.net>
2014-01-28 20:07:58 -0800

    Initial transfer

---
+++ src/NuGet.Services.Platform.Client/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("NuGet.Services.Platform.Client")]
+[assembly: AssemblyDescription("Common Client tools for the NuGet Service Platform")]
