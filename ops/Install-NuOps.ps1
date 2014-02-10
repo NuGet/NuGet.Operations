f4843c7e18e3988653e77efd30782914b1c5d594
anurse <andrew@andrewnurse.net>
2014-02-10 12:06:46 -0800

    Added simple install script for NuOps

---
+++ ops/Install-NuOps.ps1
@@ -0,0 +1,59 @@
+Write-Host "NuOps Installer"
+Write-Host "This installer will ask you a few small questions, then set up the NuOps environment on your machine"
+
+function Prompt($msg, $default) {
+    do {
+        $val = Read-Host "$msg [$default]"
+    } while([String]::IsNullOrEmpty($val))
+}
+
+function Confirm() {
+    param($Message, [bool]$Default = $true)
+
+    $choices = "[Y/n]"
+    if(!$Default) {
+        $choices = "[y/N]"
+    }
+
+    $val = $null
+    do {
+        $str = Read-Host "$Message $choices"
+        if([String]::IsNullOrWhitespace($str)) {
+            $val = $Default
+        } elseif([String]::Equals($str, "y", "OrdinalIgnoreCase") -or [String]::Equals($str, "yes", "OrdinalIgnoreCase")) {
+            $val = $true
+        } elseif([String]::Equals($str, "n", "OrdinalIgnoreCase") -or [String]::Equals($str, "no", "OrdinalIgnoreCase")) {
+            $val = $false
+        } else {
+            $val = $null
+        }
+    } while($val -eq $null)
+    $val
+}
+
+if([String]::IsNullOrWhitespace($env:NUOPS_APP_MODEL) -or (!(Test-Path $env:NUOPS_APP_MODEL))) {
+    $DefaultInternalRepository = Join-Path (Split-Path -Parent (Split-Path -Parent $PSScriptRoot)) "Deployment"
+
+    Write-Host "In order to use NuOps, you must clone your application's deployment configuration repository."
+    $location = Prompt "Where have you cloned this Repository?" $DefaultInternalRepository
+
+    $appModel = Join-Path $location "AppModel.xml"
+    if(!(Test-Path $appModel)) {
+        throw "Could not find AppModel.xml file in this repository! Make sure you've cloned the config repository first!"
+    }
+
+    Write-Host "Setting NUOPS_APP_MODEL environment variable to $appModel"
+    [Environment]::SetEnvironmentVariable("NUOPS_APP_MODEL", $appModel, "User")
+    $env:NUOPS_APP_MODEL = $appModel
+}
+
+# Check for Azure
+Import-Module -ErrorVariable moderr Azure
+
+if($moderr) {
+    Write-Host "Azure PowerShell is not installed."
+    Write-Host "Go to https://go.microsoft.com/fwlink/p/?linkid=320376&clcid=0x409 to install it"
+    if(Confirm "Launch browser to that URL?") {
+        Start-Process "https://go.microsoft.com/fwlink/p/?linkid=320376&clcid=0x409"
+    }
+}
