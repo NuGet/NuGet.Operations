004675d821763bb67e6c640037036119fe6f52f3
Deepak Aravindakshan <daravind@microsoft.com>
2014-02-12 15:05:17 -0800

    Added SyncPackagesInFailoverDC job 1. Copies packages from package backups container to packages container in    Failover DC if the package is not already present or if there is a newer revision of the    package caused by 'Edit Metadata' 2. Deletes packages from packages container in failover DC which are not    found in the database, i.e, delete from packages in primary DC
    Need to use the sourceUri overload on StartCopyFromAsync
    by setting up a SharedAccessKey. Otherwise, it won't copy across
    datacenters
    
    1. Tested that the job copies packages from package backups container to
       packages container cross account and cross DC
    2. Tested that the job deletes packages from packages container in failover
       DC that are not present in the Database

--- src/Work/NuGet.Services.Work/Jobs/Models/PackageRef.cs
+++ src/Work/NuGet.Services.Work/Jobs/Models/PackageRef.cs
@@ -8,8 +8,25 @@ namespace NuGet.Services.Work.Jobs.Models
 {
     public class PackageRef
     {
+        public PackageRef(string id, string version, string hash)
+        {
+            Id = id;
+            Version = version;
+            Hash = hash;
+        }
         public string Id { get; set; }
         public string Version { get; set; }
         public string Hash { get; set; }
+    }
+
+    public class PackageRefWithLastEdited : PackageRef
+    {
+        public PackageRefWithLastEdited(string id, string version, string hash, DateTime lastEdited)
+            : base(id, version, hash)
+        {
+            LastEdited = lastEdited;
+        }
+
+        public DateTime LastEdited { get; set; }
     }
 }
