fc54d3e435bf742d9319a454c2f42a23f82f51a2
anurse <andrew@andrewnurse.net>
2014-06-09 10:43:45 -0700

    Fixed some shadow-copying issues

--- ops/Modules/NuGetOps/Private/Load-Session.ps1
+++ ops/Modules/NuGetOps/Private/Load-Session.ps1
@@ -1,24 +1,32 @@
+$asms = @(
+    "NuGet.Services.Operations.dll",
+    "RazorEngine.dll",
+    "Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
+)
+
 function Load-Session {
     param($NuGetAppModel)
 
     # Try to load the assembly containing the types
-    $OpsAssembly = Join-Path (Convert-Path "$PSScriptRoot\..\..\..\..\src\NuGet.Services.Operations") "bin\Debug\NuGet.Services.Operations.dll"
-    if(!(Test-Path $OpsAssembly)) {
+    $OpsAsmRoot = Join-Path (Convert-Path "$PSScriptRoot\..\..\..\..\src\NuGet.Services.Operations") "bin\Debug"
+    if(!(Test-Path "$OpsAsmRoot\NuGet.Services.Operations.dll")) {
         throw "Unable to load environments. NuGet.Services.Operations has not been built."
     }
 
-    # Shadow-copy the assembly
+    # Shadow-copy the assembly and dependencies
     $tmp = Join-Path ([IO.Path]::GetTempPath()) "NuOps"
     if(!(Test-Path $tmp)) {
         mkdir $tmp | Out-Null
     }
-    $asm = Join-Path $tmp "NuGet.Services.Operations.dll"
-    if(Test-Path $asm) {
-        rm -for $asm
+    $asms | ForEach-Object {
+        $asm = Join-Path $tmp $_
+        if(Test-Path $asm) {
+            rm -for $asm
+        }
+        cp "$OpsAsmRoot\$_" $asm
     }
-    cp $OpsAssembly $asm
 
-    Add-Type -Path $asm
+    Add-Type -Path "$tmp\NuGet.Services.Operations.dll"
 
     $session = [NuGet.Services.Operations.OperationsSession]::Load((Convert-Path $NuGetAppModel));
     $session
